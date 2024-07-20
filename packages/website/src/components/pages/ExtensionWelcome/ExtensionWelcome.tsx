41f954022df74bd1dc5d52f95ed41dcada5203f0
Mircea Trofimciuc <mircea.trofimciuc@gmail.com>
2024-07-20 08:47:09 -0500

    fix typo

--- packages/website/src/components/pages/ExtensionWelcome/ExtensionWelcome.tsx
+++ packages/website/src/components/pages/ExtensionWelcome/ExtensionWelcome.tsx
@@ -14,7 +14,7 @@ import {
 import { useSession } from "next-auth/react";
 import { useState } from "react";
 import { Brand } from "extension/src/components/atoms/Brand/Brand";
-import { theme } from "extension//src/components/theme";
+import { theme } from "extension/src/components/theme";
 import { extensionName } from "extension//src/config";
 
 const Triangle = () => {
